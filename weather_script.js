da2b58bb6c55151662de373f754cb1bfabbb1806
Dylan Jackson <dyjackson90@gmail.com>
2021-07-27 18:05:28 -0400

    js update

---
+++ weather_script.js
@@ -0,0 +1,21 @@
+const api = {
+  key: "d8f6bf093b1bbaac360aa0e4fb188359",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+        console.log(searchbox.value);
+    }
+}
+
+function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
